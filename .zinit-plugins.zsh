4670580e3b391340bd87cbcc678bf8a2a06032fe
Ken Chu <chuwingyu@gmail.com>
2020-03-20 20:53:04 +0800

    init commit

---
+++ .zinit-plugins.zsh
@@ -0,0 +1,110 @@
+# zinit annexes
+zinit light-mode for \
+      zinit-zsh/z-a-rust
+
+# ----------------
+# Productive Tools
+# ----------------
+
+# fast-syntax-highlighting, zsh-completions, zsh-autosuggestions
+zinit wait lucid for \
+      atinit="ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+      zdharma/fast-syntax-highlighting \
+      blockf \
+      zsh-users/zsh-completions \
+      atload="!_zsh_autosuggest_start" \
+      zsh-users/zsh-autosuggestions
+
+# zaw, zsh-autopair
+zinit wait lucid for \
+      zsh-users/zaw \
+      hlissner/zsh-autopair
+
+# fasd
+zinit wait lucid for \
+      make="PREFIX=$ZPFX install" \
+      sbin="fasd" clvv/fasd \
+      svn OMZ::plugins/fasd
+
+# fzf, fzy, fzf-marks
+zinit pack="bgn+keys" git for fzf
+# zinit pack="bgn" git for fzy
+zinit wait lucid for \
+      urbainvaes/fzf-marks \
+      svn OMZ::plugins/zsh-interactive-cd
+
+# bat, fd, exa
+zinit wait="1" lucid from="gh-r" as="null" for \
+      sbin="**/bat"       @sharkdp/bat \
+      sbin="**/fd"        @sharkdp/fd \
+      sbin="exa* -> exa"  ogham/exa \
+      sbin="direnv* -> direnv" direnv/direnv
+
+# zinit wait="1" as="program" \
+#       make='!' atclone='./direnv hook zsh > zhook.zsh' \
+#       atpull='%atclone' pick="direnv" src="zhook.zsh" for \
+#       direnv/direnv
+
+zinit svn for \
+      PZT::modules/utility \
+      PZT::modules/completion \
+      PZT::modules/directory \
+      PZT::modules/osx \
+      PZT::modules/git \
+      OMZ::plugins/history-substring-search \
+      OMZ::plugins/tmux
+
+
+# Container Tools
+zinit wait="1" lucid svn for \
+      PZT::modules/docker \
+      OMZ::plugins/kubectl \
+      OMZ::plugins/ansible \
+      OMZ::plugins/colored-man-pages
+
+zinit wait="2" svn for \
+      PZT::modules/python \
+      PZT::modules/ruby \
+      PZT::modules/node
+
+# -----------------------------
+# programming languages related
+# -----------------------------
+
+# Installation of Rust compiler environment via the z-a-rust annex
+zinit wait lucid as="null" id-as="rust" sbin="bin/*" rustup \
+      atload="[[ ! -f ${ZINIT[COMPLETIONS_DIR]}/_cargo ]] \
+                 && zi creinstall -q rust; \
+                 export CARGO_HOME=\$PWD; \
+                 export RUSTUP_HOME=\$PWD/rustup" for \
+      zdharma/null
+
+# nvm, rvm, pyenv, pipenv
+zinit wait pack="bgn" git for pyenv
+# zinit pack param="GEM → colorls" for any-gem
+# zinit pack param="MOD → prettier" for any-node
+
+
+# -----------------------
+# zsh powerlevel10k theme
+# -----------------------
+
+# zinit ice depth=1; zinit light romkatv/powerlevel10k
+
+# Load within zshrc – for the instant prompt
+zinit ice nocd lucid atload='!source ~/.p10k.zsh'
+zinit load romkatv/powerlevel10k
+
+# Load ~/.p10k_zinit.zsh when in ~/github/zinit.git
+zinit id-as='zinit-prompt' nocd lucid \
+      unload='[[ $PWD != */zinit.git(|/*) ]]' \
+      load='![[ $PWD = */zinit.git(|/*) ]]' \
+      atload='!source ~/.p10k_lean.zsh; _p9k_precmd' for \
+      zdharma/null
+
+# Load ~/.p10k.zsh when in any other directory
+zinit id-as='normal-prompt' nocd lucid \
+      unload='[[ $PWD = */zinit.git(|/*) ]]' \
+      load='![[ $PWD != */zinit.git(|/*) ]]' \
+      atload='!source ~/.p10k.zsh; _p9k_precmd' for \
+      zdharma/null
