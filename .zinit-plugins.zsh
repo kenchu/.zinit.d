ec319037ab7b5371793d75a681ee19dbf6e0758d
Ken Chu <chuwingyu@gmail.com>
2020-04-17 01:49:25 +0800

    code formatting

--- .zinit-plugins.zsh
+++ .zinit-plugins.zsh
@@ -15,8 +15,7 @@ zinit wait lucid for \
       atload="!_zsh_autosuggest_start" \
       zsh-users/zsh-autosuggestions
 
-zinit for \
-      zsh-users/zsh-history-substring-search
+zinit for zsh-users/zsh-history-substring-search
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 
